8ca10603c4be433c39624b629232e1218d45a17d
ZenaGamingsky <ZenaGamingsky@gmail.com>
2015-11-20 15:38:28 +0000

    Update RawRabbit.php

--- src/pocketmine/item/RawRabbit.php
+++ src/pocketmine/item/RawRabbit.php
@@ -18,7 +18,7 @@
  *
 */
 namespace pocketmine\item;
-class RawChicken extends Item{
+class RawRabbit extends Item{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::RAW_RABBIT, $meta, $count, "Raw Rabbit");
 	}
