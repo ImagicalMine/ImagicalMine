57b16a8da1692e9efdbb93cf1b44f1b21bce0bb9
Mr. Piggy <mcpepig123@gmail.com>
2015-11-25 23:24:01 -0800

    :3

--- src/pocketmine/item/BeetrootSoup.php
+++ src/pocketmine/item/BeetrootSoup.php
@@ -22,7 +22,7 @@
 namespace pocketmine\item;
 
 
-class BeetrootSoup extends Item{
+class BeetrootSoup extends Item implements Food{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::BEETROOT_SOUP, 0, $count, "Beetroot Soup");
 	}
