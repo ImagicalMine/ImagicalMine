8e05f248ee7a46fb59dfa95e10fd380a746052f9
Mr. Piggy <mcpepig123@gmail.com>
2015-11-26 10:13:24 -0800

    Update MushroomStew.php

--- src/pocketmine/item/MushroomStew.php
+++ src/pocketmine/item/MushroomStew.php
@@ -22,7 +22,7 @@
 namespace pocketmine\item;
 
 
-class MushroomStew extends Item{
+class MushroomStew extends Item implements Food{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::MUSHROOM_STEW, 0, $count, "Mushroom Stew");
 	}
