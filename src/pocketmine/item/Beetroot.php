f4487a51e7bc477c434f7e0a7b071b98d5e31316
Dan <thebigsmilexd@gmail.com>
2015-11-26 08:27:45 +0100

    Update Beetroot.php
    Can someone continue? Its horrible with phone!

--- src/pocketmine/item/Beetroot.php
+++ src/pocketmine/item/Beetroot.php
@@ -21,7 +21,7 @@
 
 namespace pocketmine\item;
 
-class Beetroot extends Item{
+class Beetroot extends Item implements Food{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::BEETROOT, $meta, $count, "Beetroot");
 	}
