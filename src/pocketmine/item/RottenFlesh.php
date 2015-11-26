1ac4387588f8677d55970f01f6fbd4698035babe
Mr. Piggy <mcpepig123@gmail.com>
2015-11-25 23:25:16 -0800

    Update RottenFlesh.php

--- src/pocketmine/item/RottenFlesh.php
+++ src/pocketmine/item/RottenFlesh.php
@@ -22,7 +22,7 @@
 namespace pocketmine\item;
 
 
-class RottenFlesh extends Item{
+class RottenFlesh extends Item implements Food{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::ROTTEN_FLESH, 0, $count, "Rotten Flesh");
 	}
