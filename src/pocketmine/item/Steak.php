f649f4941c8935578cb343673be6b0178fbe90f7
Mr. Piggy <mcpepig123@gmail.com>
2015-11-25 23:21:08 -0800

    Helpful piggy lel

--- src/pocketmine/item/Steak.php
+++ src/pocketmine/item/Steak.php
@@ -21,7 +21,7 @@
 
 namespace pocketmine\item;
 
-class Steak extends Item{
+class Steak extends Item implements Food{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::STEAK, $meta, $count, "Steak");
 	}
