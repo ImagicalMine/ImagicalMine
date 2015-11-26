cfc362d7aa2e72e6803f50fbdfb31cef1b0ee386
Mr. Piggy <mcpepig123@gmail.com>
2015-11-26 10:12:24 -0800

    Update PumpkinPie.php

--- src/pocketmine/item/PumpkinPie.php
+++ src/pocketmine/item/PumpkinPie.php
@@ -21,7 +21,7 @@
 
 namespace pocketmine\item;
 
-class PumpkinPie extends Item{
+class PumpkinPie extends Item implements Food{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::PUMPKIN_PIE, $meta, $count, "Pumpkin Pie");
 	}
