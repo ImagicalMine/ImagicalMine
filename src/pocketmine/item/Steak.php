790d59c85d85a2deebbe05c7538e2808d01e0336
Driesboy <d.coenen@telenet.be>
2015-11-27 11:10:50 +0100

    Update Steak.php

--- src/pocketmine/item/Steak.php
+++ src/pocketmine/item/Steak.php
@@ -25,6 +25,8 @@ class Steak extends Item implements Food{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::STEAK, $meta, $count, "Steak");
 	}
-	public function isEatable(){}
+	public function isEatable() {
+		return true;
+	}
 	
 }
