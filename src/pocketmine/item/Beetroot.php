4ef4ec9973cf0f15d629594bfcc0b660900c843f
Driesboy <d.coenen@telenet.be>
2015-11-27 07:32:12 +0100

    Update Beetroot.php

--- src/pocketmine/item/Beetroot.php
+++ src/pocketmine/item/Beetroot.php
@@ -25,6 +25,8 @@ class Beetroot extends Item implements Food{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::BEETROOT, $meta, $count, "Beetroot");
 	}
-	public function isEatable(){}
+	public function isEatable() {
+		return true;
+	}
 	
 }
