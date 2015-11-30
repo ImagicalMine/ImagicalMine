a532a7cf2837e0e6cf10f018081f7b296c8418f3
Dan <thebigsmilexd@gmail.com>
2015-11-30 13:02:21 +0100

    Oops

--- src/pocketmine/block/TrappedChest.php
+++ src/pocketmine/block/TrappedChest.php
@@ -167,7 +167,7 @@ class TrappedChest extends Transparent{
 			if($player->isCreative()){
 				return true;
 			}
-			$chest!==null?$player->addWindow($chest->getInventory()):return true;
+			$chest!==null?$player->addWindow($chest->getInventory()):false;
 		}
 
 		return true;
