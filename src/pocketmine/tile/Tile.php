95d790ca641194b21be1558da9d479dc52240c9c
thebigsmileXD <thebigsmilexd@gmail.com>
2015-12-01 13:15:39 +0100

    Tile formatting, fixed FlowerPot
    Maybe we'll have to change things about the FlowerPot. I weren't sure if
    i need to write it into /tile or into /block

--- src/pocketmine/tile/Tile.php
+++ src/pocketmine/tile/Tile.php
@@ -43,7 +43,7 @@ abstract class Tile extends Position{
 	const SKULL = "Skull";
 	const BREWING_STAND = "Cauldron";
 	const ENCHANT_TABLE = "EnchantTable";
-        const TRAPPED_CHEST = "TrappedChest";
+	const TRAPPED_CHEST = "TrappedChest";
 
 	public static $tileCount = 1;
 
