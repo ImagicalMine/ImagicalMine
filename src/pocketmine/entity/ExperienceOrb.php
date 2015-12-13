95592915bc1b923bc513b8e9d9b82c436621ca5a
Mr. Piggy <mcpepig123@gmail.com>
2015-12-13 15:42:36 -0800

    Dumb spacing

--- src/pocketmine/entity/ExperienceOrb.php
+++ src/pocketmine/entity/ExperienceOrb.php
@@ -36,75 +36,75 @@ class ExperienceOrb extends Entity{
     const NETWORK_ID = 69;
 
     public $collected = false;
-	protected $amount = 0;
+    protected $amount = 0;
 
-	public function __construct(FullChunk $chunk, $nbt){
-		parent::__construct($chunk, $nbt);
+    public function __construct(FullChunk $chunk, $nbt){
+	parent::__construct($chunk, $nbt);
+    }
+
+    public function onUpdate($currentTick){
+	if($this->closed){
+		return false;
 	}
-
-	public function onUpdate($currentTick){
-		if($this->closed){
-			return false;
-		}
 		
-		$this->timings->startTiming();
+	$this->timings->startTiming();
 		
-		$hasUpdate = parent::onUpdate($currentTick);
-		$collector = null;
+	$hasUpdate = parent::onUpdate($currentTick);
+	$collector = null;
 
-		foreach($this->getLevel()->getPlayers() as $p){
-			if(!$this->collected){
-				if($this->distance($p) < 7){  //6 or less
-					$collector = $p;
-					$this->collected = true;
-				}
+	foreach($this->getLevel()->getPlayers() as $p){
+		if(!$this->collected){
+			if($this->distance($p) < 7){  //6 or less
+				$collector = $p;
+				$this->collected = true;
 			}
 		}
+	}
 
-		if($this->age > 1200 || $this->collected){
-			$this->kill();
-			$hasUpdate = true;
-		}
+	if($this->age > 1200 || $this->collected){
+		$this->kill();
+		$hasUpdate = true;
+	}
 		
-		if($collector !== null){
-		    $collector->giveExp($this->getAmount());
+	if($collector !== null){
+		$collector->giveExp($this->getAmount());
         }
 
-		$this->timings->stopTiming();
+	$this->timings->stopTiming();
 		
-		return $hasUpdate;
-	}
+        return $hasUpdate;
+    }
 
-	public function spawnTo(Player $player) {
-		$pk = new AddEntityPacket();
-		$pk->type = ExperienceOrb::NETWORK_ID;
-		$pk->eid = $this->getId();
-		$pk->x = $this->x;
-		$pk->y = $this->y;
-		$pk->z = $this->z;
-		$pk->speedX = $this->motionX;
-		$pk->speedY = $this->motionY;
-		$pk->speedZ = $this->motionZ;
-		$pk->metadata = $this->dataProperties;
-		$player->dataPacket($pk->setChannel(Network::CHANNEL_ENTITY_SPAWNING));
-		/*
-		$pk = new SpawnExperienceOrbPacket();
-		$pk->eid = $this->getId();
-		$pk->x = $this->x;
-		$pk->y = $this->y;
-		$pk->z = $this->z;
-		$pk->count = $this->getAmount();
-		$player->dataPacket($pk->setChannel(Network::CHANNEL_ENTITY_SPAWNING));
-		*/
-		
-		parent::spawnTo($player);
-	}
+    public function spawnTo(Player $player) {
+	$pk = new AddEntityPacket();
+	$pk->type = ExperienceOrb::NETWORK_ID;
+	$pk->eid = $this->getId();
+	$pk->x = $this->x;
+	$pk->y = $this->y;
+	$pk->z = $this->z;
+	$pk->speedX = $this->motionX;
+	$pk->speedY = $this->motionY;
+	$pk->speedZ = $this->motionZ;
+	$pk->metadata = $this->dataProperties;
+	$player->dataPacket($pk->setChannel(Network::CHANNEL_ENTITY_SPAWNING));
+	/*
+	$pk = new SpawnExperienceOrbPacket();
+	$pk->eid = $this->getId();
+	$pk->x = $this->x;
+	$pk->y = $this->y;
+	$pk->z = $this->z;
+	$pk->count = $this->getAmount();
+	$player->dataPacket($pk->setChannel(Network::CHANNEL_ENTITY_SPAWNING));
+	*/
+	
+        parent::spawnTo($player);
+    }
 
-	public function getAmount(){
-		return $this->amount;
-	}
+    public function getAmount(){
+	return $this->amount;
+    }
 
-	public function setAmount($amount){
-		$this->amount = $amount;
-	}
+    public function setAmount($amount){
+	$this->amount = $amount;
+    }
 }
