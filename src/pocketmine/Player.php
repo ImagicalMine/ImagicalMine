17b3023d455a67a5fc1c281b6ced177247d7a2df
Mr. Piggy <mcpepig123@gmail.com>
2015-12-22 10:20:17 -0800

    Might fix #315

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -3290,7 +3290,7 @@ Item::APPLE => 4,Item::MUSHROOM_STEW => 6,Item::BEETROOT_SOUP => 5,Item::BREAD =
 		if($this->spawned === true){
 			$this->foodTick = 0;
 			$this->getAttribute()->getAttribute(AttributeManager::MAX_HEALTH)->setValue($amount);
- 			if($amount <= 0){		 	
+ 			if($amount < 0 or $amount == 0){		 	
 			        $pk = new RespawnPacket();	
 				$pos = $this->getSpawn();		
 				$pk->x = $pos->x;		
@@ -3427,7 +3427,7 @@ Item::APPLE => 4,Item::MUSHROOM_STEW => 6,Item::BEETROOT_SOUP => 5,Item::BREAD =
 			$pk->eid = 0;
 			$pk->event = EntityEventPacket::HURT_ANIMATION;
 			$this->dataPacket($pk);
-	                if($this->getHealth() <= 0){
+	                if($this->getHealth() < 0 or $this->getHealth() == 0){
                                $pk = new RespawnPacket();
                                $pos = $this->getSpawn();
                                $pk->x = $pos->x;
