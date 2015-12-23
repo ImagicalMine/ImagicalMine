f3c707b6f93e70cab6906c620a7ef4156d098e40
Merry Keithy 2015 <keithkfng@users.noreply.github.com>
2015-12-23 21:40:35 +1100

    Update reminder if installation not found

--- start.cmd
+++ start.cmd
@@ -15,7 +15,7 @@ if exist ImagicalMine.phar (
 	if exist src\pocketmine\PocketMine.php (
 		set POCKETMINE_FILE=src\pocketmine\PocketMine.php
 	) else (
-		echo "Couldn't find a valid ImagicalMine installation"
+		echo "Couldn't find a valid ImagicalMine installation. If you have recently upgraded, ensure that you have renamed PocketMine-MP.phar to ImagicalMine.phar"
 		pause
 		exit 1
 	)
