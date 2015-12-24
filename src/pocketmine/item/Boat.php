54af2f14b076fa46d61013677fabe99570e8976b
ZZM <1013452574@qq.com>
2015-12-25 01:45:08 +0800

    Update Boat.php

--- src/pocketmine/item/Boat.php
+++ src/pocketmine/item/Boat.php
@@ -46,5 +46,47 @@ class Boat extends Item{
 	public function getMaxStackSize(){
 		return 1;
 	}
+	
+	public function canBeActivated(){
+		return true;
+	}
+
+	public function onActivate(Level $level, Player $player, Block $block, Block $target, $face, $fx, $fy, $fz){
+		$realPos = $block->getSide($face);
+
+		$boat = new BoatEntity($player->getLevel()->getChunk($realPos->getX() >> 4, $realPos->getZ() >> 4), new Compound("", [
+			"Pos" => new Enum("Pos", [
+				new Double("", $realPos->getX()),
+				new Double("", $realPos->getY()),
+				new Double("", $realPos->getZ())
+			]),
+			"Motion" => new Enum("Motion", [
+				new Double("", 0),
+				new Double("", 0),
+				new Double("", 0)
+			]),
+			"Rotation" => new Enum("Rotation", [
+				new Float("", 0),
+				new Float("", 0)
+			]),
+		]));
+		$boat->spawnToAll();
+
+		if($player->isSurvival()) {
+			$item = $player->getInventory()->getItemInHand();
+			$count = $item->getCount();
+			if(--$count <= 0){
+				$player->getInventory()->setItemInHand(Item::get(Item::AIR));
+				return;
+			}
+
+			$item->setCount($count);
+			$player->getInventory()->setItemInHand($item);
+		}
+		
+		return true;
+	}
+}
+
 
 }
