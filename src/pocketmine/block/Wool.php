30729ccaa0a0837c0fdf894622dff21a2ba921f1
PocketKiller <tahasalah585@gmail.com>
2015-12-30 14:48:32 +0300

    wut?
    my first commit using GitHub Desktop :wink:

--- src/pocketmine/block/Wool.php
+++ src/pocketmine/block/Wool.php
@@ -13,13 +13,13 @@
  * 
  * This program is a third party build by ImagicalMine.
  * 
- * PocketMine is free software: you can redistribute it and/or modify
+ * ImagicalMine is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * @author ImagicalMine Team
- * @link http://forums.imagicalcorp.ml/
+ * @link http://forums.imagicalcorp.me/
  * 
  *
 */
