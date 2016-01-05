d059eb18371251b74837c61c5f4aa49503d2d9ea
thebigsmileXD <thebigsmilexd@gmail.com>
2016-01-05 12:50:03 +0100

    Rail block placement try
    Remember: this is an untested try. Comment below if you find issues with
    that, or if that works.

--- src/pocketmine/block/RailBlock.php
+++ src/pocketmine/block/RailBlock.php
@@ -2,17 +2,17 @@
 
 /*
  *
- *  _                       _           _ __  __ _             
- * (_)                     (_)         | |  \/  (_)            
- *  _ _ __ ___   __ _  __ _ _  ___ __ _| | \  / |_ _ __   ___  
- * | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \ 
- * | | | | | | | (_| | (_| | | (_| (_| | | |  | | | | | |  __/ 
- * |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_|  |_|_|_| |_|\___| 
- *                     __/ |                                   
- *                    |___/                                                                     
- * 
+ * _ _ _ __ __ _
+ * (_) (_) | | \/ (_)
+ * _ _ __ ___ __ _ __ _ _ ___ __ _| | \ / |_ _ __ ___
+ * | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \
+ * | | | | | | | (_| | (_| | | (_| (_| | | | | | | | | | __/
+ * |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_| |_|_|_| |_|\___|
+ * __/ |
+ * |___/
+ *
  * This program is a third party build by ImagicalMine.
- * 
+ *
  * PocketMine is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -20,151 +20,175 @@
  *
  * @author ImagicalMine Team
  * @link http://forums.imagicalcorp.ml/
- * 
  *
-*/
+ *
+ */
 
 namespace pocketmine\block;
 
 use pocketmine\math\Vector3;
 use pocketmine\item\Item;
 use pocketmine\Player;
-abstract class RailBlock extends Flowable{
 
+abstract class RailBlock extends Flowable{
 	const SIDE_NORTH_WEST = 6;
 	const SIDE_NORTH_EAST = 7;
 	const SIDE_SOUTH_EAST = 8;
 	const SIDE_SOUTH_WEST = 9;
 
 	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
-		$down=$block->getSide(Vector3::SIDE_DOWN);
+		$down = $block->getSide(Vector3::SIDE_DOWN);
 		if($down->isTransparent() === false){
-			$this->getLevel()->setBlock($this, Block::get($this->id,0));
-			$up = $block->getSide(Vector3::SIDE_UP);
-			if($block->getSide(Vector3::SIDE_EAST) && $block->getSide(Vector3::SIDE_SOUTH)){
-				$this->setDirection(self::SIDE_SOUTH_EAST);
-			}
-			elseif($block->getSide(Vector3::SIDE_EAST) && $block->getSide(Vector3::SIDE_NORTH)){
-				$this->setDirection(self::SIDE_NORTH_EAST);
-			}
-			elseif($block->getSide(Vector3::SIDE_SOUTH) && $block->getSide(Vector3::SIDE_WEST)){
-				$this->setDirection(self::SIDE_SOUTH_WEST);
-			}
-			elseif($block->getSide(Vector3::SIDE_NORTH) && $block->getSide(Vector3::SIDE_WEST)){
-				$this->setDirection(self::SIDE_NORTH_WEST);
-			}
-			elseif($block->getSide(Vector3::SIDE_EAST) && $block->getSide(Vector3::SIDE_WEST)){
-				if($up->getSide(Vector3::SIDE_EAST)){
-					$this->setDirection(Vector3::SIDE_EAST, true);
-				}
-				elseif($up->getSide(Vector3::SIDE_WEST)){
-					$this->setDirection(Vector3::SIDE_WEST, true);
-				}
-				else{
-					$this->setDirection(Vector3::SIDE_EAST);
-				}
-			}
-			elseif($block->getSide(Vector3::SIDE_SOUTH) && $block->getSide(Vector3::SIDE_NORTH)){
-				if($up->getSide(Vector3::SIDE_SOUTH)){
-					$this->setDirection(Vector3::SIDE_SOUTH, true);
-				}
-				elseif($up->getSide(Vector3::SIDE_NORTH)){
-					$this->setDirection(Vector3::SIDE_NORTH, true);
-				}
-				else{
-					$this->setDirection(Vector3::SIDE_SOUTH);
-				}
-			}
-			else{
-				$this->setDirection(Vector3::SIDE_NORTH);
-			}
+			$this->getLevel()->setBlock($this, Item::get($this->id, $this->calcDirection($block), 1));
 			return true;
 		}
 		return false;
 	}
-	
+
+	public function calcDirection($block){
+		$up = $block->getSide(Vector3::SIDE_UP);
+		if($block->getSide(Vector3::SIDE_EAST) && $block->getSide(Vector3::SIDE_SOUTH)){
+			return $this->setDirection(self::SIDE_SOUTH_EAST);
+		}
+		elseif($block->getSide(Vector3::SIDE_EAST) && $block->getSide(Vector3::SIDE_NORTH)){
+			return $this->setDirection(self::SIDE_NORTH_EAST);
+		}
+		elseif($block->getSide(Vector3::SIDE_SOUTH) && $block->getSide(Vector3::SIDE_WEST)){
+			return $this->setDirection(self::SIDE_SOUTH_WEST);
+		}
+		elseif($block->getSide(Vector3::SIDE_NORTH) && $block->getSide(Vector3::SIDE_WEST)){
+			return $this->setDirection(self::SIDE_NORTH_WEST);
+		}
+		elseif($block->getSide(Vector3::SIDE_EAST) && $block->getSide(Vector3::SIDE_WEST)){
+			if($up->getSide(Vector3::SIDE_EAST)){
+				return $this->setDirection(Vector3::SIDE_EAST, true);
+			}
+			elseif($up->getSide(Vector3::SIDE_WEST)){
+				return $this->setDirection(Vector3::SIDE_WEST, true);
+			}
+			else{
+				return $this->setDirection(Vector3::SIDE_EAST);
+			}
+		}
+		elseif($block->getSide(Vector3::SIDE_SOUTH) && $block->getSide(Vector3::SIDE_NORTH)){
+			if($up->getSide(Vector3::SIDE_SOUTH)){
+				return $this->setDirection(Vector3::SIDE_SOUTH, true);
+			}
+			elseif($up->getSide(Vector3::SIDE_NORTH)){
+				return $this->setDirection(Vector3::SIDE_NORTH, true);
+			}
+			else{
+				return $this->setDirection(Vector3::SIDE_SOUTH);
+			}
+		}
+		else{
+			return $this->setDirection(Vector3::SIDE_NORTH);
+		}
+	}
+
 	public function getDirection(){
-		switch ($this->meta){
-			case 0:{
-				return Vector3::SIDE_SOUTH;
-			}
-			case 1:{
-				return Vector3::SIDE_EAST;
-			}
-			case 2:{
-				return Vector3::SIDE_EAST;
-			}
-			case 3:{
-				return Vector3::SIDE_WEST;
-			}
-			case 4:{
-				return Vector3::SIDE_NORTH;
-			}
-			case 5:{
-				return Vector3::SIDE_SOUTH;
-			}
-			case 6:{
-				return self::SIDE_NORTH_WEST;
-			}
-			case 7:{
-				return self::SIDE_NORTH_EAST;
-			}
-			case 8:{
-				return self::SIDE_SOUTH_EAST;
-			}
-			case 9:{
-				return self::SIDE_SOUTH_WEST;
-			}
-			default:{
-				return Vector3::SIDE_SOUTH;
-			}
+		switch($this->meta){
+			case 0:
+				{
+					return Vector3::SIDE_SOUTH;
+				}
+			case 1:
+				{
+					return Vector3::SIDE_EAST;
+				}
+			case 2:
+				{
+					return Vector3::SIDE_EAST;
+				}
+			case 3:
+				{
+					return Vector3::SIDE_WEST;
+				}
+			case 4:
+				{
+					return Vector3::SIDE_NORTH;
+				}
+			case 5:
+				{
+					return Vector3::SIDE_SOUTH;
+				}
+			case 6:
+				{
+					return self::SIDE_NORTH_WEST;
+				}
+			case 7:
+				{
+					return self::SIDE_NORTH_EAST;
+				}
+			case 8:
+				{
+					return self::SIDE_SOUTH_EAST;
+				}
+			case 9:
+				{
+					return self::SIDE_SOUTH_WEST;
+				}
+			default:
+				{
+					return Vector3::SIDE_SOUTH;
+				}
 		}
 	}
-	
+
 	public function __toString(){
-		$this->getName()." facing ".$this->getDirection().($this->isCurve()?" on a curve ":($this->isOnSlope()?" on a slope":""));
+		$this->getName() . " facing " . $this->getDirection() . ($this->isCurve()?" on a curve ":($this->isOnSlope()?" on a slope":""));
 	}
-	
-	public function setDirection($face, $isOnSlope=false){
+
+	public function setDirection($face, $isOnSlope = false){
 		switch($face){
-			case Vector3::SIDE_EAST:{
-				$this->meta = $isOnSlope?2:1;
-			}
-			case Vector3::SIDE_WEST:{
-				$this->meta = $isOnSlope?3:1;
-			}
-			case Vector3::SIDE_NORTH:{
-				$this->meta = $isOnSlope?4:0;
-			}
-			case Vector3::SIDE_SOUTH:{
-				$this->meta = $isOnSlope?5:0;
-			}
-			case self::SIDE_NORTH_WEST:{
-				$this->meta = 6;
-			}
-			case self::SIDE_NORTH_EAST:{
-				$this->meta = 7;
-			}
-			case self::SIDE_SOUTH_EAST:{
-				$this->meta = 8;
-			}
-			case self::SIDE_SOUTH_WEST:{
-				$this->meta = 9;
-			}
-			default:{
-				$this->meta = 0;
-			}
+			case Vector3::SIDE_EAST:
+				{
+					$this->meta = $isOnSlope?2:1;
+				}
+			case Vector3::SIDE_WEST:
+				{
+					$this->meta = $isOnSlope?3:1;
+				}
+			case Vector3::SIDE_NORTH:
+				{
+					$this->meta = $isOnSlope?4:0;
+				}
+			case Vector3::SIDE_SOUTH:
+				{
+					$this->meta = $isOnSlope?5:0;
+				}
+			case self::SIDE_NORTH_WEST:
+				{
+					$this->meta = 6;
+				}
+			case self::SIDE_NORTH_EAST:
+				{
+					$this->meta = 7;
+				}
+			case self::SIDE_SOUTH_EAST:
+				{
+					$this->meta = 8;
+				}
+			case self::SIDE_SOUTH_WEST:
+				{
+					$this->meta = 9;
+				}
+			default:
+				{
+					$this->meta = 0;
+				}
 		}
-		$this->getLevel()->setBlock($this, Block::get($this->id, $this->meta));
+		// $this->getLevel()->setBlock($this, Block::get($this->id, $this->meta));
+		return $this->meta;
 	}
-	
+
 	public function isOnSlope(){
-		$d=$this->meta;
-		return ($d==0x02||$d==0x03||$d==0x04||$d==0x05);
+		$d = $this->meta;
+		return ($d == 0x02 || $d == 0x03 || $d == 0x04 || $d == 0x05);
 	}
-	
+
 	public function isCurve(){
-		$d=$this->meta;
-		return ($d==0x06||$d==0x07||$d==0x08||$d==0x09);
+		$d = $this->meta;
+		return ($d == 0x06 || $d == 0x07 || $d == 0x08 || $d == 0x09);
 	}
 }
