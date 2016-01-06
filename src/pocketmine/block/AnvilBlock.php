4cd65827b850cbe9bc8a1e339d44c57ec63a8a2c
ZombiePower <aneeshverma6@gmail.com>
2016-01-06 12:21:40 +1100

    now anvil can be broken

--- src/pocketmine/block/AnvilBlock.php
+++ src/pocketmine/block/AnvilBlock.php
@@ -93,7 +93,9 @@ class AnvilBlock extends Fallable{
 
         return true;
     }
-
+    public function isBreakable(Item $item){
+        return true;
+    }
     public function getDrops(Item $item){
         $damage = $this->getDamage();
         if($item->isPickaxe() >= Tool::TIER_WOODEN){ // TODO break level
