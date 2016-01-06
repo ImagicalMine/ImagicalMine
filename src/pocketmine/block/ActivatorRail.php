ce7fc0f10546fe1e7ecedb516a7e4bd12af6df81
thebigsmileXD <thebigsmilexd@gmail.com>
2016-01-06 09:26:01 +0100

    .. Well, buggy, but better than none xD

--- src/pocketmine/block/ActivatorRail.php
+++ src/pocketmine/block/ActivatorRail.php
@@ -29,7 +29,6 @@ namespace pocketmine\block;
 use pocketmine\item\Item;
 use pocketmine\item\Tool;
 use pocketmine\level\Level;
-use pocketmine\level\generator\normal\biome\PlainBiome;
 use pocketmine\Player;
 use pocketmine\math\Vector3;
 
