b22a416e8e7eea16cfea76978455b22d587f20e6
ZombiePower <aneeshverma6@gmail.com>
2016-01-06 12:26:29 +1100

    Tall Grasses can be placed on dirt and farmlands (yellowy grass)
    bug listed..

--- src/pocketmine/block/TallGrass.php
+++ src/pocketmine/block/TallGrass.php
@@ -58,7 +58,7 @@ class TallGrass extends Flowable{
 	
 	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
 		$down = $this->getSide(0);
-		if($down->getId() === self::GRASS){
+		if($down->getId() === self::GRASS or $down->getId() === self::DIRT or $down->getId() === self::FARMLAND){
 			$this->getLevel()->setBlock($block, $this, true);
 
 			return true;
