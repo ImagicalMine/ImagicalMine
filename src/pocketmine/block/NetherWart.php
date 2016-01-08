1e7f97c343ce2426cbe75b00b89134917fe8caca
ZombiePower <aneeshverma6@gmail.com>
2016-01-08 17:45:41 +1100

    better if i add/fix this in nethercops

--- src/pocketmine/block/NetherWart.php
+++ src/pocketmine/block/NetherWart.php
@@ -30,14 +30,6 @@ class NetherWart extends NetherCrops{
 	public function getName(){
 		return "Nether Wart Block";
 	}
-	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){ 
-		$down = $this->getSide(0); 
-		if($down->getId() === self::SOUL_SAND){ 
-			$this->getLevel()->setBlock($block, $this, true, true); 
-			return true; 
-		} 
-		return false; 
-	} //TODO:: If near lava, Netherwarts will grow slightly quicker
         public function getDrops(Item $item){
         $drops = [];
         if($this->meta >= 0x03){
