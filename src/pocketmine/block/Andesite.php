945a10ea96ffcbffc0f4c8637cb45495cf4d8c66
ZombiePower <aneeshverma6@gmail.com>
2016-01-09 12:41:26 +1100

    Andesite drop fix

--- src/pocketmine/block/Andesite.php
+++ src/pocketmine/block/Andesite.php
@@ -52,7 +52,7 @@ class Andesite extends Solid{
 	public function getDrops(Item $item){
 		if($item->isPickaxe() >= Tool::TIER_WOODEN){
 			return [
-				[$this->getDamage() === 0 ? Item::COBBLESTONE : Item::STONE, $this->getDamage(), 1],
+				[Item::ANDESITE, $this->meta, 1],
 			];
 		}else{
 			return [];
