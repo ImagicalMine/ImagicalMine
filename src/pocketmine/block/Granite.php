fbc946732e69047996def0d630cc52e6d2470e8a
ZombiePower <aneeshverma6@gmail.com>
2016-01-09 12:43:13 +1100

    Granite drop fix
    all was tested before i made the commit

--- src/pocketmine/block/Granite.php
+++ src/pocketmine/block/Granite.php
@@ -52,7 +52,7 @@ class Granite extends Solid{
 	public function getDrops(Item $item){
 		if($item->isPickaxe() >= Tool::TIER_WOODEN){
 			return [
-				[$this->getDamage() === 0 ? Item::COBBLESTONE : Item::STONE, $this->getDamage(), 1],
+				[Item::GRANITE, $this->meta, 1],
 			];
 		}else{
 			return [];
