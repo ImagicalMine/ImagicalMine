1cdf016936b7a1414834e0f160fb55073d22fad1
@ZombieP0wer <aneeshverma6@gmail.com>
2016-01-11 13:57:55 +1100

    Drop fix
    Thanks to @mcpepig

--- src/pocketmine/block/NetherReactor.php
+++ src/pocketmine/block/NetherReactor.php
@@ -49,7 +49,7 @@ class NetherReactor extends Solid{
 	public function getDrops(Item $item){
 		$drops = [];
 	if($item->isPickaxe() >= Tool::TIER_WOODEN){ 
- 			$drops[] = [Item::DIAMOND_INGOT, 0, 3];
+ 			$drops[] = [Item::DIAMOND, 0, 3];
  			$drops[] = [Item::IRON_INGOT, 0, 6];
 
  		} 
