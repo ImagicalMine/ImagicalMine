1039e5152add00872d0ad3854c50b1d4a63d7c14
Keith The Koder <keithkfng@users.noreply.github.com>
2016-01-12 20:42:46 +1100

    This'll probably work better

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -1,7 +1,7 @@
 <?php
 // this is a test to disable xdebug on Linux. If it doesn't work, it'll be removed
-exec('sudo sed -i 's/xdebug.profiler_enable=1/xdebug.profiler_enable=0/g' /etc/php5/conf.d/xdebug.ini');
-exec('sudo service apache2 restart');
+exec('sudo php5dismod xdebug');
+exec('sudo service php5-fpm restart');
 
 namespace {
 	function safe_var_dump(){
