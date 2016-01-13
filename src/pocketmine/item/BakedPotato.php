12c815f9714d8cc366001984406b6309c21fc913
wrewolf <wrewolf@gmail.com>
2016-01-13 23:33:05 +0300

    Update BakedPotato.php

--- src/pocketmine/item/BakedPotato.php
+++ src/pocketmine/item/BakedPotato.php
@@ -7,4 +7,8 @@ class BakedPotato extends Food{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::BAKED_POTATO, $meta, $count, "Baked Potato");
 	}
+
+        public function getSmeltingExp(){
+            return 0.35;
+        }
 }
