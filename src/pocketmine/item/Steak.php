41640881db3fd5bd35c2fff08ffbf6485a1e9339
wrewolf <wrewolf@gmail.com>
2016-01-13 23:29:23 +0300

    Update Steak.php

--- src/pocketmine/item/Steak.php
+++ src/pocketmine/item/Steak.php
@@ -7,4 +7,8 @@ class Steak extends Food{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::STEAK, $meta, $count, "Steak");
 	}
+
+        public function getSmeltingExp(){
+            return 0.35;
+        }
 }
