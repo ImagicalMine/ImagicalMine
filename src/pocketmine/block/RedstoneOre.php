461479e834e2e63244ae416199d82934eb3733e4
@ZombieP0wer <aneeshverma6@gmail.com>
2016-01-20 16:25:49 +1100

    Redstone Ore Tweak

--- src/pocketmine/block/RedstoneOre.php
+++ src/pocketmine/block/RedstoneOre.php
@@ -63,7 +63,7 @@ class RedstoneOre extends Solid{
 	}
 
 	public function getDrops(Item $item){
-		if($item->isPickaxe() >= Tool::TIER_GOLD){
+		if($item->isPickaxe() >= Tool::TIER_IRON){
 			return [
 				[Item::REDSTONE_DUST, 0, mt_rand(4, 5)],
 			];
