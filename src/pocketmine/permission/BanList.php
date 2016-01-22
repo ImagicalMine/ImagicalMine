5d747b7305ec169b7b3cec3792fc42956e72b2c4
Ragnok123 <gebab.urumci.kv@gmail.com>
2016-01-23 00:48:27 +0100

    BanList update

--- src/pocketmine/permission/BanList.php
+++ src/pocketmine/permission/BanList.php
@@ -25,67 +25,54 @@
 */
 
 namespace pocketmine\permission;
-
 use pocketmine\Server;
 use pocketmine\utils\MainLogger;
-
 class BanList{
-
 	/** @var BanEntry[] */
 	private $list = [];
-
 	/** @var string */
 	private $file;
-
 	/** @var bool */
-	private $enabled = true;
-
+	private $enabled = \true;
 	/**
 	 * @param string $file
 	 */
 	public function __construct($file){
 		$this->file = $file;
 	}
-
 	/**
 	 * @return bool
 	 */
 	public function isEnabled(){
-		return $this->enabled === true;
+		return $this->enabled === \true;
 	}
-
 	/**
 	 * @param bool $flag
 	 */
 	public function setEnabled($flag){
 		$this->enabled = (bool) $flag;
 	}
-
 	/**
 	 * @return BanEntry[]
 	 */
 	public function getEntries(){
 		$this->removeExpired();
-
 		return $this->list;
 	}
-
 	/**
 	 * @param string $name
 	 *
 	 * @return bool
 	 */
 	public function isBanned($name){
-		$name = strtolower($name);
+		$name = \strtolower($name);
 		if(!$this->isEnabled()){
-			return false;
+			return \false;
 		}else{
 			$this->removeExpired();
-
 			return isset($this->list[$name]);
 		}
 	}
-
 	/**
 	 * @param BanEntry $entry
 	 */
@@ -93,7 +80,6 @@ class BanList{
 		$this->list[$entry->getName()] = $entry;
 		$this->save();
 	}
-
 	/**
 	 * @param string    $target
 	 * @param string    $reason
@@ -102,29 +88,25 @@ class BanList{
 	 *
 	 * @return BanEntry
 	 */
-	public function addBan($target, $reason = null, $expires = null, $source = null){
+	public function addBan($target, $reason = \null, $expires = \null, $source = \null){
 		$entry = new BanEntry($target);
-		$entry->setSource($source != null ? $source : $entry->getSource());
+		$entry->setSource($source != \null ? $source : $entry->getSource());
 		$entry->setExpires($expires);
-		$entry->setReason($reason != null ? $reason : $entry->getReason());
-
+		$entry->setReason($reason != \null ? $reason : $entry->getReason());
 		$this->list[$entry->getName()] = $entry;
 		$this->save();
-
 		return $entry;
 	}
-
 	/**
 	 * @param string $name
 	 */
 	public function remove($name){
-		$name = strtolower($name);
+		$name = \strtolower($name);
 		if(isset($this->list[$name])){
 			unset($this->list[$name]);
 			$this->save();
 		}
 	}
-
 	public function removeExpired(){
 		foreach($this->list as $name => $entry){
 			if($entry->hasExpired()){
@@ -132,12 +114,11 @@ class BanList{
 			}
 		}
 	}
-
 	public function load(){
 		$this->list = [];
-		$fp = @fopen($this->file, "r");
-		if(is_resource($fp)){
-			while(($line = fgets($fp)) !== false){
+		$fp = @\fopen($this->file, "r");
+		if(\is_resource($fp)){
+			while(($line = \fgets($fp)) !== \false){
 				if($line{0} !== "#"){
 					$entry = BanEntry::fromString($line);
 					if($entry instanceof BanEntry){
@@ -145,28 +126,25 @@ class BanList{
 					}
 				}
 			}
-			fclose($fp);
+			\fclose($fp);
 		}else{
 			MainLogger::getLogger()->error("Could not load ban list");
 		}
 	}
-
-	public function save($flag = true){
+	public function save($flag = \true){
 		$this->removeExpired();
-		$fp = @fopen($this->file, "w");
-		if(is_resource($fp)){
-			if($flag === true){
-				fwrite($fp, "# Updated " . strftime("%x %H:%M", time()) . " by " . Server::getInstance()->getName() . " " . Server::getInstance()->getPocketMineVersion() . "\n");
-				fwrite($fp, "# victim name | ban date | banned by | banned until | reason\n\n");
+		$fp = @\fopen($this->file, "w");
+		if(\is_resource($fp)){
+			if($flag === \true){
+				\fwrite($fp, "# Updated " . \strftime("%x %H:%M", \time()) . " by " . Server::getInstance()->getName() . " " . Server::getInstance()->getPocketMineVersion() . "\n");
+				\fwrite($fp, "# victim name | ban date | banned by | banned until | reason\n\n");
 			}
-
 			foreach($this->list as $entry){
-				fwrite($fp, $entry->getString() . "\n");
+				\fwrite($fp, $entry->getString() . "\n");
 			}
-			fclose($fp);
+			\fclose($fp);
 		}else{
 			MainLogger::getLogger()->error("Could not save ban list");
 		}
 	}
-
 }
