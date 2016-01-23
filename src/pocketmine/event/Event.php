e5fdff62d711b2384f06c61c48c9fe1180e49133
Ragnok123 <gebab.urumci.kv@gmail.com>
2016-01-23 01:01:35 +0100

    Event update

--- src/pocketmine/event/Event.php
+++ src/pocketmine/event/Event.php
@@ -28,29 +28,24 @@
  * Event related classes
  */
 namespace pocketmine\event;
-
 abstract class Event{
-
 	/**
 	 * Any callable event must declare the static variable
 	 *
-	 * public static $handlerList = null;
+	 * public static $handlerList = \null;
 	 * public static $eventPool = [];
 	 * public static $nextEvent = 0;
 	 *
 	 * Not doing so will deny the proper event initialization
 	 */
-
-	protected $eventName = null;
-	private $isCancelled = false;
-
+	protected $eventName = \null;
+	private $isCancelled = \false;
 	/**
 	 * @return string
 	 */
 	final public function getEventName(){
-		return $this->eventName === null ? get_class($this) : $this->eventName;
+		return $this->eventName === \null ? \get_class($this) : $this->eventName;
 	}
-
 	/**
 	 * @return bool
 	 *
@@ -60,11 +55,9 @@ abstract class Event{
 		if(!($this instanceof Cancellable)){
 			throw new \BadMethodCallException("Event is not Cancellable");
 		}
-
 		/** @var Event $this */
-		return $this->isCancelled === true;
+		return $this->isCancelled === \true;
 	}
-
 	/**
 	 * @param bool $value
 	 *
@@ -72,24 +65,20 @@ abstract class Event{
 	 *
 	 * @throws \BadMethodCallException
 	 */
-	public function setCancelled($value = true){
+	public function setCancelled($value = \true){
 		if(!($this instanceof Cancellable)){
 			throw new \BadMethodCallException("Event is not Cancellable");
 		}
-
 		/** @var Event $this */
 		$this->isCancelled = (bool) $value;
 	}
-
 	/**
 	 * @return HandlerList
 	 */
 	public function getHandlers(){
-		if(static::$handlerList === null){
+		if(static::$handlerList === \null){
 			static::$handlerList = new HandlerList();
 		}
-
 		return static::$handlerList;
 	}
-
 }
