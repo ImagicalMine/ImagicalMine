0ecf46d6f86b8b4e12e1244a21e331f16f5224f1
Ronald Schmidt <ronald.schmidt@zu-web.de>
2016-02-09 10:33:58 +0100

    revert - jenkins download quit slow

--- JenkinsBuildStart.sh
+++ JenkinsBuildStart.sh
@@ -1,7 +1,7 @@
 # wget https://bintray.com/artifact/download/pocketmine/PocketMine/PHP_7.0.3_x86-64_Linux.tar.gz
-# tar zxf PHP_7.0.3_x86-64_Linux.tar.gz
-wget https://github.com/ecoron/ImagicalMine-php7-linux/releases/download/0.1/ImagicalMine-php7-linux.tar.gz
-tar zxf ImagicalMine-php7-linux.tar.gz
+tar zxf PHP_7.0.3_x86-64_Linux.tar.gz
+# wget https://github.com/ecoron/ImagicalMine-php7-linux/releases/download/0.1/ImagicalMine-php7-linux.tar.gz
+# tar zxf ImagicalMine-php7-linux.tar.gz
 
 if [ ! -d "plugins" ]; then  
 mkdir "plugins"  
