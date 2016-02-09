9fb1c4b89e03f776c9fe4081c25b1a15281560b4
Keith @ ImagicalMine <keithkfng@users.noreply.github.com>
2016-02-09 16:19:54 +1100

    Some updates

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -1,29 +1,5 @@
 <?php
 
-/*
- *
- *  _                       _           _ __  __ _
- * (_)                     (_)         | |  \/  (_)
- *  _ _ __ ___   __ _  __ _ _  ___ __ _| | \  / |_ _ __   ___
- * | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \
- * | | | | | | | (_| | (_| | | (_| (_| | | |  | | | | | |  __/
- * |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_|  |_|_|_| |_|\___|
- *                     __/ |
- *                    |___/
- *
- * This program is a third party build by ImagicalMine.
- *
- * ImagicalMine is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author ImagicalMine Team
- * @link http://forums.imagicalcorp.ml/
- *
- *
-*/
-
 namespace {
 	function safe_var_dump(){
 		static $cnt = 0;
@@ -80,8 +56,8 @@ namespace pocketmine {
 	const VERSION = "1.0dev";
 	const API_VERSION = "1.14.0";
 	const CODENAME = "ImagicalMine";
-	const MINECRAFT_VERSION = "v0.14.0.4 alpha";
-	const MINECRAFT_VERSION_NETWORK = "0.14.0.4";
+	const MINECRAFT_VERSION = "v0.14.x alpha";
+	const MINECRAFT_VERSION_NETWORK = "0.14.0";
 
 	/*
 	 * Startup code. Do not look at it, it may harm you.
