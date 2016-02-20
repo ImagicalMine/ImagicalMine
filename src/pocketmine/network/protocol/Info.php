b09bd74b8c2e7c5255597cf4691f9fa4c61ea3e6
TheDeibo <deiboibbs@gmail.com>
2016-02-20 00:20:03 +0000

    Update Info.php
    ImagicalMine is now Available for 0.14.x. I briefly tested it. It works.

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -36,7 +36,7 @@ interface Info{
 	 * Actual Minecraft: PE protocol version
 	 */
 	const CURRENT_PROTOCOL = 45;
-	const ACCEPTED_PROTOCOLS = [40,41,42,43,44,45,46];
+    const ACCEPTED_PROTOCOLS = [40,41,42,43,44,45,46,47];
 
 	const LOGIN_PACKET = 0x8f;
 	const PLAY_STATUS_PACKET = 0x90;
@@ -96,14 +96,3 @@ interface Info{
     const ATTRIBUTE_PACKET = 0x60;
 
 }
-
-
-
-
-
-
-
-
-
-
-
