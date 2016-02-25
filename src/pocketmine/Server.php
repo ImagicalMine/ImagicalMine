2fc819e10969a89c7c6fd4f9b91a3f58cee3060f
Keith @ ImagicalMine <keithkfng@users.noreply.github.com>
2016-02-25 21:26:41 +1100

    Fix my really stupid error
    As I am always blind LOL

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -1602,7 +1602,7 @@ class Server{
 	        $this->autoTickRateLimit = (int) $this->getProperty("level-settings.auto-tick-rate-limit", 20);
 	        $this->alwaysTickPlayers = (int) $this->getProperty("level-settings.always-tick-players", false);
 	        $this->baseTickRate = (int) $this->getProperty("level-settings.base-tick-rate", 1);
-		$this->chunkRadius = $this->getAdvancedProperty("level-settings.chunk-radius", 30);
+		$this->chunkRadius = (int) $this->getAdvancedProperty("level-settings.chunk-radius", 30);
 
 	        $this->scheduler = new ServerScheduler();
 
