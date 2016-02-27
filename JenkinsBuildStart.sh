b4e80aa260f1194c8cb2634c8bcbfe26ca3b997c
Keith @ ImagicalMine <keithkfng@users.noreply.github.com>
2016-02-28 10:06:16 +1100

    Update JenkinsBuildStart.sh

--- JenkinsBuildStart.sh
+++ JenkinsBuildStart.sh
@@ -1,5 +1,5 @@
-# wget https://bintray.com/artifact/download/pocketmine/PocketMine/PHP_7.0.3_x86-64_Linux.tar.gz
-# tar zxf PHP_7.0.3_x86-64_Linux.tar.gz
+wget https://bintray.com/artifact/download/pocketmine/PocketMine/PHP_7.0.3_x86-64_Linux.tar.gz
+tar zxf PHP_7.0.3_x86-64_Linux.tar.gz
 # wget https://github.com/ecoron/ImagicalMine-php7-linux/releases/download/0.1/ImagicalMine-php7-linux.tar.gz
 # tar zxf ImagicalMine-php7-linux.tar.gz
 
@@ -10,7 +10,7 @@ if [ ! -d "releases" ]; then
 mkdir "releases"  
 fi
 PHP_BINARY="./bin/php7/bin/php"
-# wget -O plugins/DevTools.phar https://github.com/PocketMine/DevTools/releases/download/v1.9.0/DevTools_v1.9.0.phar
+wget -O plugins/DevTools.phar https://github.com/PocketMine/DevTools/releases/download/v1.9.0/DevTools_v1.9.0.phar
 # cp /etc/php5-pm/DevTools_v1.10.0.phar ./plugins/DevTools.phar
 chmod +x JenkinsBuild.php
 ./bin/php7/bin/php JenkinsBuild.php
