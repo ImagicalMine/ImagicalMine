408b38a77b3f061c7a94dceabed25c43d60d20a4
kvetinac97 <kvetinac97@users.noreply.github.com>
2016-03-03 15:28:25 +0100

    Joining fix
    When devices with less or 512 MB RAM join, they see "Building Terrain" screen and immediatly MCPE crashes. Only way how to fix that is set chunkRadius to 10 or less. (even if $server->chunkRadius is 10) it doesn't work and MCPE crashes.
    
    Note: It should be better to have option if it should be 10 (512 MB can join) or 30 (only 1024 MB and higher can join)

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -1896,7 +1896,7 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 		}
 
  		$pk = new ChunkRadiusUpdatePacket();
- 		$pk->radius = $this->server->chunkRadius;
+ 		$pk->radius = 10;
  		$this->dataPacket($pk);
 
 		$this->forceMovement = $this->teleportPosition = $this->getPosition();
