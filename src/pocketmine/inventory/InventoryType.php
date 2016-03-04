867e114dd6a73fbce710b351e64fa1767cf8a65a
BlackShadow1 <marketto_02@hotmail.it>
2016-03-04 19:50:56 +0100

    Update InventoryType.php

--- src/pocketmine/inventory/InventoryType.php
+++ src/pocketmine/inventory/InventoryType.php
@@ -40,7 +40,7 @@ class InventoryType{
 	const BREWING_STAND = 7;
 	const ANVIL = 8;
 	const ENCHANT_TABLE = 9;
-    const DISPENSER = 10;
+        const DISPENSER = 10;
 	const DROPPER = 11;
 	const HOPPER = 12;
 
@@ -73,7 +73,9 @@ class InventoryType{
 		static::$default[static::ENCHANT_TABLE] = new InventoryType(2, "Enchant", 4); //1 INPUT/OUTPUT, 1 LAPIS
 		static::$default[static::BREWING_STAND] = new InventoryType(4, "Brewing", 5); //1 INPUT, 3 POTION
 		static::$default[static::ANVIL] = new InventoryType(3, "Anvil", 6); //2 INPUT, 1 OUTPUT
-        static::$default[static::DISPENSER] = new InventoryType(36, "Dispenser", 9); //36 INPUT, 9 OUTPUT
+                static::$default[static::DISPENSER] = new InventoryType(9, "Dispenser", 6); //9 CONTAINER
+		static::$default[static::DROPPER] = new InventoryType(9, "Dropper", 7); //9 CONTAINER
+		static::$default[static::HOPPER] = new InventoryType(5, "Hopper", 8); //5 CONTAINER
 	}
 
 	/**
