7e0da7e0036b0008d27b30705ecbf42c9ff7c3b2
BlackShadow1 <marketto_02@hotmail.it>
2016-03-05 18:51:15 +0100

    Fixed some inventory can't be accessed

--- src/pocketmine/inventory/InventoryType.php
+++ src/pocketmine/inventory/InventoryType.php
@@ -70,10 +70,10 @@ class InventoryType{
 		static::$default[static::FURNACE] = new InventoryType(3, "Furnace", 2);
 		static::$default[static::CRAFTING] = new InventoryType(5, "Crafting", 1); //4 CRAFTING slots, 1 RESULT
 		static::$default[static::WORKBENCH] = new InventoryType(10, "Crafting", 1); //9 CRAFTING slots, 1 RESULT
-		static::$default[static::ENCHANT_TABLE] = new InventoryType(2, "Enchant", 4); //1 INPUT/OUTPUT, 1 LAPIS
-		static::$default[static::BREWING_STAND] = new InventoryType(4, "Brewing", 5); //1 INPUT, 3 POTION
-		static::$default[static::ANVIL] = new InventoryType(3, "Anvil", 6); //2 INPUT, 1 OUTPUT
-                static::$default[static::DISPENSER] = new InventoryType(9, "Dispenser", 6); //9 CONTAINER
+		static::$default[static::ENCHANT_TABLE] = new InventoryType(2, "Enchant", 3); //1 INPUT/OUTPUT, 1 LAPIS
+		static::$default[static::BREWING_STAND] = new InventoryType(4, "Brewing", 4); //1 INPUT, 3 POTION
+		static::$default[static::ANVIL] = new InventoryType(3, "Anvil", 5); //2 INPUT, 1 OUTPUT
+        static::$default[static::DISPENSER] = new InventoryType(9, "Dispenser", 6); //9 CONTAINER
 		static::$default[static::DROPPER] = new InventoryType(9, "Dropper", 7); //9 CONTAINER
 		static::$default[static::HOPPER] = new InventoryType(5, "Hopper", 8); //5 CONTAINER
 	}
