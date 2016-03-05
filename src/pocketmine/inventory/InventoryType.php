985609148cc97cdc152a5347c1f738447bd25bdd
Eclipse <0-Eclipse-0@users.noreply.github.com>
2016-03-05 10:41:49 +0700

    Update InventoryType.php

--- src/pocketmine/inventory/InventoryType.php
+++ src/pocketmine/inventory/InventoryType.php
@@ -41,7 +41,7 @@ class InventoryType{
 	const ANVIL = 8;
 	const ENCHANT_TABLE = 9;
         const DISPENSER = 10;
-        //const DROPPER = 11;
+        const DROPPER = 11;
 	const HOPPER = 12;
 
 	private static $default = [];
@@ -74,7 +74,7 @@ class InventoryType{
 		static::$default[static::BREWING_STAND] = new InventoryType(4, "Brewing", 5); //1 INPUT, 3 POTION
 		static::$default[static::ANVIL] = new InventoryType(3, "Anvil", 6); //2 INPUT, 1 OUTPUT
                 static::$default[static::DISPENSER] = new InventoryType(9, "Dispenser", 6); //9 CONTAINER
-		//static::$default[static::DROPPER] = new InventoryType(9, "Dropper", 7); //9 CONTAINER
+		static::$default[static::DROPPER] = new InventoryType(9, "Dropper", 7); //9 CONTAINER
 		static::$default[static::HOPPER] = new InventoryType(5, "Hopper", 8); //5 CONTAINER
 	}
 
