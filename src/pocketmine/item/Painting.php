a0a77efef164acb251ff40b07efb9e99569a70ce
BlackShadow1 <marketto_02@hotmail.it>
2016-03-05 20:16:47 +0100

    Finished #711 (From ClearSky)

--- src/pocketmine/item/Painting.php
+++ src/pocketmine/item/Painting.php
@@ -29,25 +29,27 @@ namespace pocketmine\item;
 use pocketmine\block\Block;
 use pocketmine\level\Level;
 use pocketmine\Player;
-
+use pocketmine\entity\Painting as PaintingEntity;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\nbt\tag\DoubleTag;
+use pocketmine\nbt\tag\FloatTag;
 
 class Painting extends Item{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::PAINTING, 0, $count, "Painting");
 	}
-
-	public function canBeActivated() : bool{
+	public function canBeActivated(){
 		return true;
 	}
-
 	public function onActivate(Level $level, Player $player, Block $block, Block $target, $face, $fx, $fy, $fz){
-		if($target->isTransparent() === false and $face > 1 and $block->isSolid() === false){
+		if($target->isTransparent() === false and $face !== 0 and $face !== 1 and $block->isSolid() === false){
 			$faces = [
-				2 => 1,
-				3 => 3,
-				4 => 0,
-				5 => 2,
-
+				2 => 0,
+				3 => 2,
+				4 => 1,
+				5 => 3,
 			];
 			$motives = [
 				// Motive Width Height
@@ -81,22 +83,45 @@ class Painting extends Item{
 			$motive = $motives[mt_rand(0, count($motives) - 1)];
 			$data = [
 				"x" => $target->x,
-				"y" => $target->y,
+				"y" => $target->y + 0.4,
 				"z" => $target->z,
 				"yaw" => $faces[$face] * 90,
 				"Motive" => $motive[0],
 			];
-			//TODO
-			//$e = $server->api->entity->add($level, ENTITY_OBJECT, OBJECT_PAINTING, $data);
-			//$e->spawnToAll();
-			/*if(($player->gamemode & 0x01) === 0x00){
-				$player->removeItem(Item::get($this->getId(), $this->getDamage(), 1));
+			
+			$nbt = new CompoundTag("", [
+				"Motive" => new StringTag("Motive", $data["Motive"]),
+				"Pos" => new ListTag("Pos", [
+					new DoubleTag("", $data["x"]),
+					new DoubleTag("", $data["y"]),
+					new DoubleTag("", $data["z"])
+				]),
+				"Motion" => new ListTag("Motion", [
+					new DoubleTag("", 0),
+					new DoubleTag("", 0),
+					new DoubleTag("", 0)
+				]),
+				"Rotation" => new ListTag("Rotation", [
+					new FloatTag("", $data["yaw"]),
+					new FloatTag("", 0)
+				]),
+			]);
+			
+			$painting = new PaintingEntity($player->getLevel()->getChunk($block->getX() >> 4, $block->getZ() >> 4), $nbt);
+			$painting->spawnToAll();
+			
+			/*if($player->isSurvival()){
+				$item = $player->getInventory()->getItemInHand();
+				$count = $item->getCount();
+				if(--$count <= 0){
+					$player->getInventory()->setItemInHand(Item::get(Item::AIR));
+					return;
+				}
+				$item->setCount($count);
+				$player->getInventory()->setItemInHand($item);
 			}*/
-
 			return true;
 		}
-
 		return false;
 	}
-
 }
