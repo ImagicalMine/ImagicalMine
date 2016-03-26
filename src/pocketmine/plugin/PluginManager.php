cf74d51b396e0b65562815e7bc8be54e18d47848
BlackShadow1 <marketto_02@hotmail.it>
2016-03-26 20:31:05 +0100

    Maybe I've a problem..

--- src/pocketmine/plugin/PluginManager.php
+++ src/pocketmine/plugin/PluginManager.php
@@ -239,7 +239,7 @@ class PluginManager{
 								$this->server->getLogger()->error($this->server->getLanguage()->translateString("pocketmine.plugin.loadError", [$name, "%pocketmine.plugin.incompatibleAPI"]));
 								continue;
 							}else{
-								$this->server->getLogger()->warning("Loading plugin '$name' with incompatible API version (the plugin API is not compatible with Cheetah-MP API version " . $this->server->getApiVersion() . ")");
+								$this->server->getLogger()->warning("Loading plugin '$name' with incompatible API version (the plugin API is not compatible with ImagicalMine API version " . $this->server->getApiVersion() . ")");
 							}
 
 							$plugins[$name] = $file;
