dff3fe9af376a9f5b1176d54cbb9244af5351218
Pub4Game <Pub4Game@users.noreply.github.com>
2016-03-26 19:13:29 +0300

    fix Torch

--- src/pocketmine/block/Torch.php
+++ src/pocketmine/block/Torch.php
@@ -42,7 +42,7 @@ class Torch extends Flowable{
 		return 15;
 	}
 
-	public function getName(){
+	public function getName() {
 		return "Torch";
 	}
 
@@ -61,7 +61,7 @@ class Torch extends Flowable{
 				0 => 0,
 			];
 
-			if($this->getSide($faces[$side])->isTransparent() === true and !($side === 0 and ($below->getId() === self::FENCE or $below->getId() === self::COBBLE_WALL)) or $this->getSide($faces[$side])->isSolid() === false){
+			if($this->getSide($faces[$side])->isTransparent() === true and !($side === 0 and ($below->getId() === self::FENCE or $below->getId() === self::COBBLE_WALL)){
 				$this->getLevel()->useBreakOn($this);
 
 				return Level::BLOCK_UPDATE_NORMAL;
@@ -86,7 +86,7 @@ class Torch extends Flowable{
 			$this->getLevel()->setBlock($block, $this, true, true);
 
 			return true;
-		}elseif($below->isTransparent() === false or $below->getId() === self::FENCE or $below->getId() === self::COBBLE_WALL){
+		}elseif($below->isTransparent() === false or $below->getId() === self::FENCE or	$below->getId() === self::COBBLE_WALL){
 			$this->meta = 0;
 			$this->getLevel()->setBlock($block, $this, true, true);
 
@@ -96,7 +96,7 @@ class Torch extends Flowable{
 		return false;
 	}
 
-	public function getDrops(Item $item){
+	public function getDrops(Item $item) {
 		return [
 			[$this->id, 0, 1],
 		];
