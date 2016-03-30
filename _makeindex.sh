a4966b81a8d3908989c5fe213c38e66b0a20a944
Keith <keithkfng@users.noreply.github.com>
2016-03-30 17:11:57 +1100

    Update _makeindex.sh

--- _makeindex.sh
+++ _makeindex.sh
@@ -1,4 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-ls | egrep '(phar)' | \\ #filter the content you want in the index
-perl -e 'print "<html><body><ul>"; while(<>) { chop $_;  print "<li><a href=\"./$_\">$_</a></li>";} print "</ul></body></html>"' > index.html
+ROOT=/home/travis/build/ImagicalMine
+HTTP="/"
+OUTPUT="index.html" 
+
+i=0
+echo "<UL>" > $OUTPUT
+for filepath in `find "$ROOT" -maxdepth 1 -mindepth 1 -type d| sort`; do
+  path=`basename "$filepath"`
+  echo "  <LI>$path</LI>" >> $OUTPUT
+  echo "  <UL>" >> $OUTPUT
+  for i in `find "$filepath" -maxdepth 1 -mindepth 1 -type f| sort`; do
+    file=`basename "$i"`
+    echo "    <LI><a href=\"/$path/$file\">$file</a></LI>" >> $OUTPUT
+  done
+  echo "  </UL>" >> $OUTPUT
+done
+echo "</UL>" >> $OUTPUT
