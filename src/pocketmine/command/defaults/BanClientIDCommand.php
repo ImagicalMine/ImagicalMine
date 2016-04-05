eb7831dc2a57fefc1e7b8a9f1e0ced7b18679571
BlackShadow1 <marketto_02@hotmail.it>
2016-04-05 16:08:40 +0200

    Create BanClientIDCommand.php

---
+++ src/pocketmine/command/defaults/BanClientIDCommand.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+ *
+ *  _                       _           _ __  __ _             
+ * (_)                     (_)         | |  \/  (_)            
+ *  _ _ __ ___   __ _  __ _ _  ___ __ _| | \  / |_ _ __   ___  
+ * | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \ 
+ * | | | | | | | (_| | (_| | | (_| (_| | | |  | | | | | |  __/ 
+ * |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_|  |_|_|_| |_|\___| 
+ *                     __/ |                                   
+ *                    |___/                                                                     
+ * 
+ * This program is a third party build by ImagicalMine.
+ * 
+ * PocketMine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author ImagicalMine Team
+ * @link http://forums.imagicalcorp.ml/
+ * 
+ *
+*/
+
+namespace pocketmine\command\defaults;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\TranslationContainer;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+
+class BanClientIDCommand extends VanillaCommand{
+
+	public function __construct($name){
+		parent::__construct(
+			$name,
+			"%pocketmine.command.banclient.description",
+			"%commands.banclient.usage"
+		);
+		$this->setPermission("pocketmine.command.banclient");
+	}
+
+	public function execute(CommandSender $sender, $currentAlias, array $args){
+		if(!$this->testPermission($sender)){
+			return true;
+		}
+
+		if(count($args) === 0){
+			$sender->sendMessage(new TranslationContainer("commands.generic.usage", [$this->usageMessage]));
+
+			return false;
+		}
+
+		$client = array_shift($args);
+		$reason = implode(" ", $args);
+		
+		if(is_numeric($client)){
+			foreach($sender->getServer()->getOnlinePlayers() as $p){
+				$p = $sender->getServer()->getPlayer($client);
+			    if($p->getClientId() === $client){
+				    $p->kick($reason !== "" ? "Banned by admin. Reason:" . $reason : "Banned by admin.");
+				    break;
+				}
+				$sender->getServer()->getClientBans()->addBan($client, $reason, null, $sender->getName());
+			
+			    Command::broadcastCommandMessage($sender, new TranslationContainer("%commands.banclientid.success", [$p !== null ? $client : $client]));
+			
+			    return true;
+			}
+		}else{
+			foreach($sender->getServer()->getOnlinePlayers() as $p){
+				$p = $sender->getServer()->getPlayer($client);
+			    $sender->getServer()->getClientBans()->addBan($p->getClientId, $reason, null, $sender->getName()); // 
+			    $p->kick($reason !== "" ? "Banned by admin Reason: " . $reason : "Banned by admin", false);
+				
+				Command::broadcastCommandMessage($sender, new TranslationContainer("%commands.banclientid.success.players", [$p->getClientId, $p->getName()]));
+			}
+			
+			return true;
+		}
+		
+		return true;
+		
+	}
+	
+}
