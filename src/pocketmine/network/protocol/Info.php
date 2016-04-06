501a315311d80a237db0a2f17bcc9665d0dd5399
hoyinm14mc <hoyinm14@gmail.com>
2016-04-06 22:45:12 +0800

    Updated current protocol

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -35,7 +35,7 @@ interface Info{
 	/**
 	 * Actual Minecraft: PE protocol version
 	 */
-	const CURRENT_PROTOCOL = 45;
+	const CURRENT_PROTOCOL = 46;
     const ACCEPTED_PROTOCOLS = [40,41,42,43,44,45,46,47];
 
 	const LOGIN_PACKET = 0x8f;
