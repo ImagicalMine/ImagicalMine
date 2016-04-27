911474c419644065213e19828a6c3f6c16c26e7d
Keith <keithkfng@users.noreply.github.com>
2016-04-27 17:54:14 +1000

    Update for 0.14.2 - see description
    Requires testing. I don't know what protocol version it is.

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -36,7 +36,7 @@ interface Info{
 	 * Actual Minecraft: PE protocol version
 	 */
 	const CURRENT_PROTOCOL = 46;
-    	const ACCEPTED_PROTOCOLS = [40,41,42,43,44,45,46,47];
+    	const ACCEPTED_PROTOCOLS = [40,41,42,43,44,45,46,47,48,49,50];
 
 	const LOGIN_PACKET = 0x8f;
 	const PLAY_STATUS_PACKET = 0x90;
